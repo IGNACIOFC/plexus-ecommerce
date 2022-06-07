262697e54e4e022fba65a148a3795fa8c34b490f
Ignacio <ifernandezcerro@gmail.com>
2022-06-07 12:47:19 +0200

    SearchBar component logic

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import styles from './SearchBar.module.css';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+
+export default function SearchBar ({placeholder, data}) {
+
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.model.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+  return (
+    <div className={styles.search}>
+      <div className={styles.searchInputs}>
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+        <div className={styles.searchIcon}>
+          {filteredData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" onClick={clearInput} />
+          )}
+        </div>
+      </div>
+      {filteredData.length !== 0 && (
+        <div className={styles.dataResult}>
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <a className={styles.dataItem} href={value.id}>
+                <p>{value.model} </p>
+              </a>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  )
+}
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,26 +3,50 @@ import Header from '../../components/Header';
 import { fetchProducts } from '../../services/products';
 import styles from './Home.module.css';
 import ProductCard from '../../components/ProductCard';
+import SearchBar from '../../components/SearchBar';
+import { useState } from 'react';
 
 export default function Home () {
   const {data: products, isLoading} = useQuery(["products"], fetchProducts);
+  const [currentPage, setCurrentPage] = useState(0);
 
-  
+  const pageProducts = () => {
+    if(products) {
+      return products.slice(currentPage, currentPage + 8);
+    }
+  }
+
+  function nextPage() {
+    setCurrentPage(currentPage+ 5)
+  }
+
+  function prevPage() {
+    if(currentPage > 0) {
+      setCurrentPage(currentPage - 5)
+    }
+  }
 
   return (
     <div className={styles.homeContainer}>
       <Header />
       <div className={styles.home}>
-        <h2 className={styles.homeTitle}>Nuestros Productos</h2>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.homeTitle}>Nuestros Productos</h2>
+          <SearchBar placeholder={'Encuentra tu producto...'} data={products}/>
+        </div>
         {
           isLoading && <p>Loading...</p>
         }
         {
-          products && 
+          pageProducts() && 
           <div className={styles.productList}>
-            {products.map(product => <ProductCard product={product} />)}
+            {pageProducts().map(product => <ProductCard product={product}/>)}
           </div>
         }
+        <div className={styles.buttonContainer}>
+          <button onClick={prevPage}>Anteriores</button>
+          <button onClick={nextPage}>Siguientes</button>
+        </div>
       </div>
       
     </div>
