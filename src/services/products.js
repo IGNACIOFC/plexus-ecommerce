39dd45160793b9f23271a73165887cf4777fbd55
Ignacio <ifernandezcerro@gmail.com>
2022-06-09 15:50:21 +0200

    try catch api calls

--- src/services/products.js
+++ src/services/products.js
@@ -3,22 +3,38 @@ import axios from 'axios'
 const API = process.env.REACT_APP_API || 'https://front-test-api.herokuapp.com/api'
 
 export const fetchProducts = async () => {
-  const { data } = await axios.get(`${API}/product`)
-  return data
+  try {
+    const { data } = await axios.get(`${API}/product`)
+    return data
+  } catch (error) {
+    return error
+  }
 }
 
 export const fetchProductDetails = async (itemId) => {
-  const { data } = await axios.get(`${API}/product${itemId}`)
-  return data
+  try {
+    const { data } = await axios.get(`${API}/product${itemId}`)
+    return data
+  } catch (error) {
+    return error
+  }
 }
 
 export const fetchCartItems = async () => {
-  const cartItems = localStorage.getItem('cart')
-  const initialValue = JSON.parse(cartItems)
+  try {
+    const cartItems = localStorage.getItem('cart')
+    const initialValue = JSON.parse(cartItems)
 
-  return initialValue || 0
+    return initialValue || 0
+  } catch (error) {
+    return error
+  }
 }
 
 export const addToCart = async (body) => {
-  await axios.post(`${API}/cart`, body)
+  try {
+    await axios.post(`${API}/cart`, body)
+  } catch (error) {
+    return error
+  }
 }
