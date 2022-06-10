6d79fe6c42a1c6c8085137b47bfc3dbac1021c4c
Ignacio <ifernandezcerro@gmail.com>
2022-06-10 09:27:33 +0200

    fix cartIcon

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -4,19 +4,13 @@ import { useQuery } from 'react-query'
 import { fetchCartItems } from '../../services/products'
 
 export default function CartIcon () {
-  const { data: items, isLoading, isSuccess } = useQuery(['cart'], fetchCartItems)
+  const { data: items } = useQuery(['cart'], fetchCartItems)
 
-  if (isLoading) {
-    return <></>
-  }
-
-  if (isSuccess) {
-    return (
-      <div>
-        <Badge color='secondary' badgeContent={items} data-testid='icon'>
-          <ShoppingCartIcon />{' '}
-        </Badge>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Badge color='secondary' badgeContent={ items || 0 } data-testid='icon'>
+        <ShoppingCartIcon />{' '}
+      </Badge>
+    </div>
+  )
 }
